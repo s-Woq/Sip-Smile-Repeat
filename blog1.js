d32c20e95fbcbd95a57f611ee60cf163f7039908
midward <midwardisaac@gmail.com>
2023-08-02 15:25:23 -0500

    Landing page done

--- blog1.js
+++ blog1.js
@@ -5,14 +5,15 @@ boxes.forEach(box => {
     const textElement = document.createElement('span');
     textElement.textContent = box.textContent;
     textElement.classList.add('typewriter-text');
-    box.textContent = '';
-    box.appendChild(textElement);
+    box.textContent = ''; // Clear the box's content
+    box.appendChild(textElement); // Append the span to the box
   });
 
   box.addEventListener('mouseleave', () => {
     const textElement = box.querySelector('.typewriter-text');
     if (textElement) {
       box.removeChild(textElement);
+      box.textContent = textElement.textContent; // Restore original text
     }
   });
 });
