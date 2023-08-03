7dab7052420849db82666433ae4de7250dbe2965
midward <midwardisaac@gmail.com>
2023-08-03 00:21:32 -0500

    Fixing blog 1 50%

--- blog1.js
+++ blog1.js
@@ -1,19 +1,23 @@
-const boxes = document.querySelectorAll('.box');
+const boxes = document.querySelectorAll(".box");
+const contentDisplayed = document.querySelector(".content-displayed");
+const content = document.querySelector(".content");
 
-boxes.forEach(box => {
-  box.addEventListener('mouseenter', () => {
-    const textElement = document.createElement('span');
-    textElement.textContent = box.textContent;
-    textElement.classList.add('typewriter-text');
-    box.textContent = ''; // Clear the box's content
-    box.appendChild(textElement); // Append the span to the box
-  });
+const boxContents = [
+    "Aprender algo nuevo puede ser complicado debido a la carga cognitiva que impone en nuestro cerebro. Un error común es intentar memorizar sin comprender, lo que resulta en un aprendizaje superficial que dificulta la retención y aplicación.b  ",
+    "This is the content for Box 2...",
+    "This is the content for Box 3..."
+];
 
-  box.addEventListener('mouseleave', () => {
-    const textElement = box.querySelector('.typewriter-text');
-    if (textElement) {
-      box.removeChild(textElement);
-      box.textContent = textElement.textContent; // Restore original text
-    }
-  });
+boxes.forEach((box, index) => {
+    box.addEventListener("click", () => {
+        content.innerHTML = boxContents[index];
+        contentDisplayed.classList.add("active");
+    });
 });
+
+contentDisplayed.addEventListener("click", (event) => {
+    if (event.target === contentDisplayed) {
+        contentDisplayed.classList.remove("active");
+    }
+});
+
