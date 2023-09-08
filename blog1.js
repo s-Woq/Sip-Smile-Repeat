461786b903a35b293d9cd418a37c6d928b964786
midward <midwardisaac@gmail.com>
2023-09-08 16:54:16 -0500

    adding some new pics

--- blog1.js
+++ blog1.js
@@ -3,14 +3,28 @@ const contentDisplayed = document.querySelector(".content-displayed");
 const content = document.querySelector(".content");
 
 const boxContents = [
-    "<div class='blog'><img id='first-image' src='./img/forest2.jpg' alt='banner1'> </div> <div class='blog'> </div> <div class='blog'></div>",
+    // Use image URLs directly (preloading them)
+    "./img/night.jpg",
     "This is the content for Box 2...",
-    "This is the content for Box 3..."
+    "This is the content for Box 3...",
 ];
+
+// Preload images
+const preloadedImages = [];
+for (let i = 0; i < boxContents.length; i++) {
+    const image = new Image();
+    image.src = boxContents[i];
+    preloadedImages.push(image);
+}
 
 boxes.forEach((box, index) => {
     box.addEventListener("click", () => {
-        content.innerHTML = boxContents[index];
+        // Check if it's an image or other content
+        if (index === 0) {
+            content.innerHTML = `<div class='blog'><img id='first-image' loading='lazy' src='${boxContents[index]}' alt='banner1'></div>`;
+        } else {
+            content.innerHTML = boxContents[index];
+        }
         contentDisplayed.classList.add("active");
     });
 });
@@ -20,4 +34,3 @@ contentDisplayed.addEventListener("click", (event) => {
         contentDisplayed.classList.remove("active");
     }
 });
-
