1bbfb155ef9152661b0e9fa683121c090153e2a8
Midward <naxusduf@gmail.com>
2023-10-05 16:04:47 -0500

    adding some changes in the socials

--- js/blog1.js
+++ js/blog1.js
@@ -2,29 +2,23 @@ const boxes = document.querySelectorAll(".box");
 const contentDisplayed = document.querySelector(".content-displayed");
 const content = document.querySelector(".content");
 
-const boxContents = [
-    // Use image URLs directly (preloading them)
-    "./img/night.jpg",
-    "This is the content for Box 2...",
-    "This is the content for Box 3...",
-];
-
-// Preload images
-const preloadedImages = [];
-for (let i = 0; i < boxContents.length; i++) {
-    const image = new Image();
-    image.src = boxContents[i];
-    preloadedImages.push(image);
-}
-
-boxes.forEach((box, index) => {
+boxes.forEach((box) => {
     box.addEventListener("click", () => {
-        // Check if it's an image or other content
-        if (index === 0) {
-            content.innerHTML = `<div class='blog'><img id='first-image' loading='lazy' src='${boxContents[index]}' alt='banner1'></div>`;
+        const boxContent = box.querySelector(".box-content");
+        const index = boxContent.getAttribute("data-index");
+        const image = boxContent.querySelector(".box-image");
+        const text = boxContent.querySelector(".box-text");
+
+        if (index === "0") {
+            // Display image and text for Box 1
+            content.innerHTML = "";
+            content.appendChild(image.cloneNode(true));
+            content.appendChild(text.cloneNode(true));
         } else {
-            content.innerHTML = boxContents[index];
+            // Display text for other boxes
+            content.innerHTML = text.innerHTML;
         }
+        
         contentDisplayed.classList.add("active");
     });
 });
